baef1baf9734f8ad7703d5b3f3bb22c64ac683f6
Matt <matias.brigante@gmail.com>
2020-06-28 20:25:49 -0300

    minor fixes and add crypto class

--- PasswordManager/Program.cs
+++ PasswordManager/Program.cs
@@ -18,7 +18,7 @@ namespace PasswordManager
         [STAThread]
         static void Main()
         {
-            //Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             StartDemoUseFile();
